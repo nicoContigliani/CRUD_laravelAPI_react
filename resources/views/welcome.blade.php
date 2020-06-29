a2d16b033488863e412eb9a86ef7964d1d490852
Nicolas_Contigliani <nico.contigliani@gmail.com>
2020-06-29 15:46:26 -0300

    agregar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Nico
                 </div>
 
                 <div class="links">
