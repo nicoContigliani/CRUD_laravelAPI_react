2cb65ee8b206336dcf80f5b71ae9c2ae3f2534eb
Nicolas_Contigliani <nico.contigliani@gmail.com>
2020-06-29 16:15:48 -0300

    curso git

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Nico
+                    Nico....
                 </div>
 
                 <div class="links">
